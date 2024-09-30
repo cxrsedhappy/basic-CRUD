94182da87c9636813d8d523b1f9a3c9a5d51e352
cxrsedhappy <cxrsedhappy@mail.ru>
2024-09-30 14:50:17 +0300

    Changed schemas for validation

--- app/api/v1/post/schemas.py
+++ app/api/v1/post/schemas.py
@@ -1,18 +1,21 @@
-from pydantic import BaseModel, field_validator
+from datetime import datetime
+from pydantic import BaseModel, field_validator, ConfigDict
 
 
-class UserModel(BaseModel):
-    username: str
-    password: str
+class PostM(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    user_id: int
+    text: str
+    created_at: datetime
+    updated_at: datetime
 
-    @field_validator('username')
-    def username_must_be_at_least_3_chars(cls, v):
-        if len(v) < 3:
-            raise ValueError('Username must be at least 3 characters')
-        return v
 
-    @field_validator('password')
-    def password_must_be_at_least_8_chars(cls, v):
-        if len(v) < 8:
-            raise ValueError('Password must be at least 8 characters')
+class PostCreateM(BaseModel):
+    text: str
+
+    @field_validator('text')
+    def text_must_be_at_least_1_char(cls, v):
+        if len(v) < 1:
+            raise ValueError('Text must be at least 1 character')
         return v
--- app/api/v1/user/schemas.py
+++ app/api/v1/user/schemas.py
@@ -1,11 +1,36 @@
-from pydantic import BaseModel, field_validator
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, field_validator
 
 
-class PostModel(BaseModel):
-    text: str
+class UserM(BaseModel):
+    """Main User model. Includes all data"""
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    username: str
+    password: str
+    created_at: datetime
+    updated_at: datetime
 
-    @field_validator('text')
-    def text_must_be_at_least_1_char(cls, v):
-        if len(v) < 1:
-            raise ValueError('Text must be at least 1 character')
+
+class UserCreateM(BaseModel):
+    """User creates model"""
+    username: str
+    password: str
+
+    @field_validator('username')
+    def username_must_be_at_least_3_chars(cls, v):
+        if len(v) < 3:
+            raise ValueError('Username must be at least 3 characters')
         return v
+
+    @field_validator('password')
+    def password_must_be_at_least_8_chars(cls, v):
+        if len(v) < 8:
+            raise ValueError('Password must be at least 8 characters')
+        return v
+
+
+class TokenM(BaseModel):
+    """Token model for OAuth"""
+    access_token: str
+    token_type: str
